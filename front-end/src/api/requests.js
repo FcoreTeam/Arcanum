989b12522cee5b50bea1f424e8bed9f9d2cf3a66
= <=>
2025-03-19 00:28:50 +0300

    requests modified

---
+++ front-end/src/api/requests.js
@@ -0,0 +1,31 @@
+import { Cookies } from "js-cookie";
+import { io } from "socket.io"; // for chat
+
+import axios from "axios";
+
+const $api = axios.create({
+  baseURL: "http://localhost:3000/api/",
+  timeout: 10000,
+});
+
+// jwt auth there
+
+export const getGames = async () =>
+  $api.get("game/getgames").catch((err) => console.log(err));
+
+export const getGame = async () =>
+  $api.get("/game/getGame").catch((err) => console.log(err));
+
+export const sendAnswer = async (data) =>
+  $api
+    .post("/answergame", data)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+
+export const getLeaders = async () =>
+  $api.get("/getleaderboard").catch((err) => console.log(err));
+
+export const setSettings = async (data) =>
+  $api
+    .put("/update_settings", data, { withCredentials: true })
+    .catch((err) => console.log(err));
