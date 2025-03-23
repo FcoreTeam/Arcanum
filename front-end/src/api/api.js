5c4da08b07f7d371d2a077b581f46da915fd93a4
= <=>
2025-03-24 00:33:11 +0300

    faq

--- front-end/src/api/api.js
+++ front-end/src/api/api.js
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 import axios from "axios";
 
 const $api = axios.create({
-  baseURL: "http://api.zoltansgametma.ru",
+  baseURL: "https://api.zoltansgametma.ru/api",
   timeout: 10000,
   withCredentials: true,
 });
@@ -18,7 +18,7 @@ export const api = {
   getUserInfo: () => $api.get("/user/user_info"),
 };
 
-export const socket = io("ws://localhost:3001", {
+export const socket = io("wss://api.zoltansgametma.ru/api", {
   path: "/socket.io/",
   extraHeaders: {
     Authorization: `Bearer ${Cookies.get("accessToken")}`,
