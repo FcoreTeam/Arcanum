65d0e19312c54d81a2e6a1d69b572080bdbb82e2
= <=>
2025-03-25 00:45:50 +0300

    requests changed

--- front-end/src/api/api.js
+++ front-end/src/api/api.js
@@ -4,21 +4,21 @@ import { io } from "socket.io-client";
 import axios from "axios";
 
 const $api = axios.create({
-  baseURL: "https://api.zoltansgametma.ru/api",
+  baseURL: "https://api.zoltansgametma.ru",
   timeout: 10000,
   withCredentials: true,
 });
 
 export const api = {
   getLeaders: (gameId) => $api.post("/getleaderboard"),
-  getGame: () => $api.get("/game/getGame"),
-  getGames: () => $api.get("/game/getgames"),
+  getGame: () => $api.get("/games/getGame"),
+  getGames: () => $api.get("/games/getgames"),
   sendAnswer: (data) => $api.post("/answergame", data),
   setSettings: (data) => $api.put("/update_settings", data),
-  getUserInfo: () => $api.get("/user/user_info"),
+  getUserInfo: () => $api.get("/users/user_info"),
 };
 
-export const socket = io("wss://api.zoltansgametma.ru/api", {
+export const socket = io("wss://api.zoltansgametma.ru", {
   path: "/socket.io/",
   extraHeaders: {
     Authorization: `Bearer ${Cookies.get("accessToken")}`,
