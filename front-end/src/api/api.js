c061c6979256aaeffcb28cd3099e523490cec859
KorvoDron <gentlemancoderman@gmail.com>
2025-03-25 01:04:58 +0300

    asdf

--- front-end/src/api/api.js
+++ front-end/src/api/api.js
@@ -4,21 +4,21 @@ import { io } from "socket.io-client";
 import axios from "axios";
 
 const $api = axios.create({
-  baseURL: "http://api.zoltansgametma.ru",
+  baseURL: "https://api.zoltansgametma.ru",
   timeout: 10000,
   withCredentials: true,
 });
 
 export const api = {
-  getLeaders: (gameId) => $api.post("/getleaderboard"),
-  getGame: () => $api.get("/game/getGame"),
-  getGames: () => $api.get("/game/getgames"),
-  sendAnswer: (data) => $api.post("/answergame", data),
-  setSettings: (data) => $api.put("/update_settings", data),
-  getUserInfo: () => $api.get("/user/user_info"),
+  getLeaders: (gameId) => $api.post("/leaderboard/getleaderboard"),
+  getGame: () => $api.get("/games/getgame"),
+  getGames: () => $api.get("/games/getgames"),
+  sendAnswer: (data) => $api.post("/games/answergame", data),
+  setSettings: (data) => $api.post("/users/update_settings", data),
+  getUserInfo: () => $api.get("/users/user_info"),
 };
 
-export const socket = io("ws://localhost:3001", {
+export const socket = io("wss://api.zoltansgametma.ru", {
   path: "/socket.io/",
   extraHeaders: {
     Authorization: `Bearer ${Cookies.get("accessToken")}`,
