915bef9c7c7b1f06a483fa535c813358126a9245
pashtetx <vadimplej43@gmail.com>
2025-04-16 12:13:45 +0200

    fix minio

--- back-end/src/minio.py
+++ back-end/src/minio.py
@@ -6,8 +6,7 @@ async def get_minio_instance() -> Minio:
         f"{MinioSettings.host}:{MinioSettings.port}",
         access_key=MinioSettings.access_key,
         secret_key=MinioSettings.secret_key, 
-        secure=MinioSettings.server_url.startswith("https://"),
-        server_url=MinioSettings.server_url
+        server_url=MinioSettings.server_url,
     )
     if not await minio.bucket_exists("videos"):
         await minio.make_bucket("videos")
