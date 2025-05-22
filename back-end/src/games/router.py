b9b64e157e5c870d0973f06517a74707f1253b8d
y0gurt-dv <sigirt3@gmail.com>
2025-05-22 22:40:19 +0300

    fix: fix answer

--- back-end/src/games/router.py
+++ back-end/src/games/router.py
@@ -65,6 +65,8 @@ async def answer(
     game = await Game.get_or_none(id=game_id).prefetch_related("users")
     if not game:
         raise HTTPException(status_code=404, detail="Game doesn't exists")
+    print(user.subscription)
+    print(user)
     if user not in game.users and user.subscription.first().expire < datetime.now():
         raise HTTPException(status_code=403, detail="The user did not buy this game")
     if game.answer.lower() == answer.answer.lower():
