faf6ad862fcfd979db29f1661680e5818194f97d
y0gurt-dv <sigirt3@gmail.com>
2025-05-24 17:19:26 +0300

    fix: fix invoice

--- back-end/src/auth/router.py
+++ back-end/src/auth/router.py
@@ -8,7 +8,7 @@ from realtime.chat import chat_name_space
 from aiogram.utils.web_app import WebAppUser
 from aiogram.types import BufferedInputFile
 
-from .models import User
+from .models import User, Subscription
 from games.models import GameResult, Game
 
 from bot.bot import bot
@@ -26,8 +26,7 @@ GetUserIdDeps = Annotated[WebAppUser, Depends(get_user_id)]
 @auth_api_router.get("/me", response_model=UserResponse)
 async def read_user(user_id: GetUserIdDeps):
     user = await User.get(telegram_id=user_id).prefetch_related("bougth_games", "subscription")
-    subscription = await user.subscription.first()
-    print([i for i in await user.subscription.all()])
+    subscription = await Subscription.get(user=user)
     print(subscription.expire)
     print(subscription.expire >= datetime.now(timezone.utc))
     if subscription.expire >= datetime.now(timezone.utc):
